276ea0a6771ef0823e6d13ab348c538849e9af8a
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-03-12 13:53:16 +0700

    Update

--- ktp-api/src/main/java/com/example/praktikum2/service/KtpService.java
+++ ktp-api/src/main/java/com/example/praktikum2/service/KtpService.java
@@ -1,4 +1,14 @@
 package com.example.praktikum2.service;
 
+import com.example.praktikum2.model.dto.KtpAddRequest;
+import com.example.praktikum2.model.dto.KtpDto;
+
+import java.util.List;
+
 public interface KtpService {
+    KtpDto createKtp(KtpAddRequest request);
+    List<KtpDto> getAllKtp();
+    KtpDto getKtpById(Integer id);
+    KtpDto updateKtp(Integer id, KtpAddRequest request);
+    void deleteKtp(Integer id);
 }
