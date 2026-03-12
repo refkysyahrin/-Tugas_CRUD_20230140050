3c69a69d1027d30fa9cbbb727dd7c3e176183dcb
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-03-12 13:27:14 +0700

    buat KtpRepository dengan fitur pengecekan duplikasi nomor KTP

--- ktp-api/src/main/java/com/example/praktikum2/repository/KtpRepository.java
+++ ktp-api/src/main/java/com/example/praktikum2/repository/KtpRepository.java
@@ -1,4 +1,8 @@
 package com.example.praktikum2.repository;
 
-public interface KtpRepository {
+import com.example.praktikum2.model.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    boolean existsByNomorKtp(String nomorKtp);
 }
